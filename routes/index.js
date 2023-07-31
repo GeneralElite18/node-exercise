9c2fb49fd7d1998445fab7ca4a9656305119fcbc
Alex Gonzales <gonzales.alexzander18@gmail.com>
2023-07-31 08:32:58 -0600

    finished exercise

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 // TODO: import router from users.route
+import userRouter from "./user.routes";
 
 const router = express.Router();
+
+router.use("/users", userRouter);
 
 router.get("/test", (req, res) => {
   res.send("working");
